20ac5316dde0e5f668d36200eef5729bbbebda7f
Arjun Chaudhary <arjunc3652@gmail.com>
2022-02-11 16:50:51 -0800

    minor

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -12,21 +12,29 @@ public class MeepMeepTesting {
         // With a field size of 800 pixels
         MeepMeep mm = new MeepMeep(800)
                 // Set field image
-                .setBackground(MeepMeep.Background.FIELD_ULTIMATE_GOAL_DARK)
+                .setBackground(MeepMeep.Background.FIELD_FREIGHT_FRENZY)
                 // Set theme
                 .setTheme(new ColorSchemeRedDark())
                 // Background opacity from 0-1
                 .setBackgroundAlpha(1f)
                 // Set constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
-                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                .setConstraints(30, 30, Math.toRadians(60), Math.toRadians(60), 11.5)
                 .followTrajectorySequence(drive ->
-                        drive.trajectorySequenceBuilder(new Pose2d(0, 0, 0))
-                                .forward(30)
-                                .turn(Math.toRadians(90))
-                                .forward(30)
-                                .turn(Math.toRadians(90))
-                                .splineTo(new Vector2d(10, 15), 0)
-                                .turn(Math.toRadians(90))
+//                        drive.trajectorySequenceBuilder(new Pose2d(-34, 56, 0))
+                                drive.trajectorySequenceBuilder(new Pose2d(-24, 57, Math.toRadians(270)))
+                                        .lineToConstantHeading(new Vector2d(-14,45))
+                                        .lineToLinearHeading(new Pose2d(-60,54,Math.toRadians(90)) )
+                                        .forward(7.5)
+                                        .strafeRight(100)
+//
+//                                .turn(Math.toRadians(270))
+//
+//                                .splineTo(new Vector2d(0, 0), 90)
+//                                .splineToConstantHeading(new Vector2d(-13,45),Math.toRadians(270))
+//                                        //TODO DROP BOX
+//                                        .back(15)
+//                                        .splineTo(new Vector2d(-65,57), Math.toRadians(360) )
+//
                                 .build()
                 )
                 .start();
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/FirstAutonomousOpmode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/FirstAutonomousOpmode.java
@@ -4,12 +4,14 @@ import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.acmerobotics.roadrunner.trajectory.Trajectory;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
+import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+
 
 public class FirstAutonomousOpmode extends LinearOpMode {
 
     @Override
     public void runOpMode() throws InterruptedException {
-        RobotMecanumDrive drive = new RobotMecanumDrive(hardwareMap);
+        SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
         Pose2d startPose = new Pose2d(-6,6,Math.toRadians(90));
         drive.setPoseEstimate(startPose);
         Trajectory t1 = drive.trajectoryBuilder(new Pose2d()).forward(6).build();
